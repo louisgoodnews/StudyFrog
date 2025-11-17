f434df59170e7a048835bfa796d4ad0a5feb6e60
Louis Goodnews <louisgoodnews95@gmail.com>
2025-11-17 19:34:15 +0100

    Added logic files for views

---
+++ src/studyfrog/gui/views/logic/edit_view_logic.py
@@ -0,0 +1,25 @@
+"""
+Author: Louis Goodnews
+Date: 2025-11-16
+"""
+
+from typing import Final
+
+
+# ---------- Constants ---------- #
+
+
+# ---------- Functions ---------- #
+
+
+# ---------- Auto-Export ---------- #
+
+# Auto-export all non-private symbols
+__all__: Final[list[str]] = [
+    key
+    for (
+        key,
+        value,
+    ) in globals().items()
+    if not key.startswith("_") and callable(value)
+]
